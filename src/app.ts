0b9b81dd9e86ee0438870c2d116cf7b6666d4686
Jakub Andrzejewski <jakubandrzejewski@op.pl>
2024-01-01 22:46:17 +0100

    Add health check

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express, { Express } from 'express';
 import alarmController from './controllers/event.controller';
 import { errorHandler } from './error/error.handler';
 import { saveAndLogEvent } from './utils/events/event.interceptor';
+import healthController from './controllers/health.controller';
 
 const app: Express = express()
     .use(express.json())
     .use(express.urlencoded({ extended: true }))
+    .use(healthController)
     .use(saveAndLogEvent)
     .use(alarmController)
     .use(errorHandler);
